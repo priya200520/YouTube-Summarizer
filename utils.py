5cea747a2b305e14ecfb843eecee3d2a5a0cbd82
priya <ramgariyapriya@gmail.com>
2026-08-07 21:22:26 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,17 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
 def extract_video_id(url):
     video_id = url.split("v=")[1]
     return video_id
+
+
+def get_transcript(video_id):
+    transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+    text = ""
+
+    for line in transcript:
+        text = text + " " + line["text"]
+
+    return text
