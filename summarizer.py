f12f23716cad0ec8c89d67cececfc57ed592b44d
priya <ramgariyapriya@gmail.com>
2026-08-08 17:54:33 +0530

    summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -11,3 +11,36 @@ def split_transcript(transcript):
     chunks = text_splitter.split_text(transcript)
 
     return chunks
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0
+)
+
+
+def summarize_chunks(chunks):
+
+    summaries = []
+
+    for i, chunk in enumerate(chunks):
+
+        prompt = f"""
+Summarize the following part of a YouTube video.
+
+Give only the important information.
+Use simple and clear language.
+
+Video Part {i + 1}:
+
+{chunk}
+"""
+
+        response = llm.invoke(prompt)
+
+        summaries.append(response.content)
+
+    return summaries
