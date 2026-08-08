99c5599f9ede40f7885652d492e4806ae707d2da
priya <ramgariyapriya@gmail.com>
2026-08-08 17:59:23 +0530

    app

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
-from utils import extract_video_id, get_transcript, summarize_text
+
+from utils import extract_video_id, get_transcript
+from summarizer import (
+    split_transcript,
+    summarize_chunks,
+    create_final_summary
+)
+
 
 st.set_page_config(
     page_title="YouTube Summarizer",
@@ -10,24 +17,44 @@ st.title("🎥 YouTube Video Summarizer")
 
 url = st.text_input("Enter YouTube URL")
 
+
 if st.button("Summarize"):
 
     if not url:
         st.warning("Please enter a YouTube URL.")
 
     else:
+
         try:
+
+            # Step 1: Get Video ID
+            video_id = extract_video_id(url)
+
+            # Step 2: Get Transcript
             with st.spinner("Getting transcript..."):
-                video_id = extract_video_id(url)
                 transcript = get_transcript(video_id)
 
             st.success("Transcript fetched successfully!")
 
-            with st.spinner("Generating AI summary..."):
-                summary = summarize_text(transcript)
+            # Step 3: Split Transcript
+            with st.spinner("Splitting transcript into chunks..."):
+                chunks = split_transcript(transcript)
 
-            st.subheader("📝 AI Summary")
-            st.markdown(summary)
+            st.write("Total Chunks:", len(chunks))
+
+            # Step 4: Summarize Each Chunk
+            with st.spinner("Generating summaries..."):
+                summaries = summarize_chunks(chunks)
+
+            # Step 5: Create Final Summary
+            with st.spinner("Creating final summary..."):
+                final_summary = create_final_summary(summaries)
+
+            # Step 6: Display
+            st.success("Summary Generated!")
+
+            st.markdown(final_summary)
 
         except Exception as e:
+
             st.error(f"Error: {e}")
